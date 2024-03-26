982645d0c58d67b66f5a5a69375c8acf8d471029
Mohammed Taufique Shaikh <59877720+staufique@users.noreply.github.com>
2024-03-26 11:42:28 +0530

    chat room

--- chat_room/chat_room/settings.py
+++ chat_room/chat_room/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'fontawesomefree',
 ]
 
 MIDDLEWARE = [
@@ -118,8 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
